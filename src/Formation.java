08bc827c36bdcf067f173c6af65506b6e2dba2d9
RUDONI Antonin <antonin.rudoni3@etu.univ-lorraine.fr>
2021-09-29 16:38:33 +0200

    coef

--- src/Formation.java
+++ src/Formation.java
@@ -25,6 +25,10 @@ public class Formation {
         }
     }
 
+    public int getMatiereCoef(String matiere){
+        return this.listeMatiere.get(matiere);
+    }
+
 
 
     public String getId() {
