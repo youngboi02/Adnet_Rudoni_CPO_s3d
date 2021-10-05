b03d3cd569dbe493fe4f0a984556db1412c719d1
adnet <sm4sH!bac>
2021-10-05 14:41:40 +0200

    ajout de la javadoc aux tests

--- src/test/TestFormation.java
+++ src/test/TestFormation.java
@@ -27,6 +27,7 @@ public class TestFormation {
         form = new Formation("matiere", map);
     }
 
+    // test du constructeur
     @Test
     public void TestConstructeur(){
         res = true;
@@ -34,12 +35,14 @@ public class TestFormation {
         assertTrue("res devrait etre true", res);
     }
 
+    // test si la matiere existe dans la hashmap
     @Test
     public void TestMatiereExsitante(){
         res = map.containsKey("maths");
         assertTrue("res devrait etre true", res);
     }
 
+    // test pour ajouter une matiere dans la hashmap
     @Test
     public void TestAjouter(){
         map.put("philo", 12);
@@ -47,21 +50,18 @@ public class TestFormation {
         assertEquals("philo devrait avoir un coeff de 12",12, oh);
     }
 
+    // test de suppression avec 2 arguments pour etre sur de cibler la bonne matiere
     @Test
     public void TestSupprimer2Args(){
         map.remove("maths",3);
         assertFalse(map.containsKey("maths"));
     }
 
+    // test de suppression general
     @Test
     public void TestSupprimer(){
         map.remove("maths");
         assertFalse(map.containsKey("maths"));
     }
-
-
-
-
-
 
 }
